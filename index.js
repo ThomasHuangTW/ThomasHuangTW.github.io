cd717de11da52d32bf85b9ca47f89ebcaa6fb545
ThomasHuangTW <thomas.publicto@gmail.com>
2025-06-28 03:12:41 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const p = document.querySelector('p');
 let xhr = new XMLHttpRequest();
+//xhr.withCredentials = true;
+//xhr.addEventListener("readystatechange", () => {
+//if(this.readyState == 4) {
+//    console.log(this.responseText);
+//}
+
+//xhr.setRequesrtHeader("contex-type", "application/x-www-form-urlencoded");
+//xhr.setRequesrtHeader("cache-control", "n0-cache");
+//xhr.setRequesrtHeader("postman-token", "");
+
 xhr.onreadystatechange = () => {
     if(xhr.readyState == 4) {
         if(xhr.status == 200) {
